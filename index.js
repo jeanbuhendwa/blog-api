c59d9786c18af7c2b158f7fa2fc5a7336ff72ecc
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-03 13:46:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,36 @@ app.post("/post", uploadmiddleware.single("file"), async (req, res) => {
   });
 });
 
+app.put("/post", uploadmiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) == JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("You are not the author");
+    }
+
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+    res.json(postDoc);
+  });
+});
+
 app.get("/post", async (req, res) => {
   res.json(
     await Post.find()
